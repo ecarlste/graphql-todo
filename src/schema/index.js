fde3a4e90615c7b8ec4c406d9997164517600383
Erik S. Carlsten <ecarlste@gmail.com>
2019-12-01 13:52:49 -0800

    Cleaned up imports in schema builder

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,11 +1,8 @@
 import { SchemaComposer } from 'graphql-compose';
-
-import db from '../utils/db'; // eslint-disable-line no-unused-vars
-
-const schemaComposer = new SchemaComposer();
-
 import { UserQuery, UserMutation } from './user';
 import { TaskQuery, TaskMutation } from './task';
+
+const schemaComposer = new SchemaComposer();
 
 schemaComposer.Query.addFields({
   ...UserQuery,
